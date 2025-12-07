ab906780d161a5258f4d19fa4a71a9f69e6edd5a
cholholim <holim225@gmail.com>
2025-12-08 00:48:22 +0900

    백 시작

--- lib/soundMap.ts
+++
@@ -1,6 +0,0 @@
-// lib/soundMap.ts
-import type { EmotionKey } from './emotionData';
-
-// 지금은 BGM 파일이 없으니까, 전부 비워둔다.
-// 나중에 mp3 준비되면 여기다가 require(...) 추가하면 됨.
-export const EmotionBgm: Partial<Record<EmotionKey, any>> = {};
